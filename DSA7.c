d770999fa8c0a82256cdb7c53465e5d42054b18d
Pranav Gadage <pranavgadage30@gmail.com>
2026-04-21 22:28:40 +0530

    Add files via upload

---
+++ DSA7.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if (left < n && arr[largest] < arr[left]) {
+        largest = left;
+    }
+
+    if (right < n && arr[largest] < arr[right]) {
+        largest = right;
+    }
+
+    if (i != largest) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        heapify(arr, n, largest);
+    }
+}
+
+void heapsort(int arr[], int n) {
+    for (int i = n/2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    for (int i = n-1; i > 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        heapify(arr, i, 0);
+    }
+}
+
+int main() {
+    int arr[] = {2, 10, 5, 4, 3};
+    int n = 5;
+
+    printf("Before sorting:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    heapsort(arr, n);
+
+    printf("\nAfter sorting:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
